81917e1f9e5150eccec4a393500b83d558aed52e
User <User@DESKTOP-LER0GJO>
2024-01-26 20:03:12 -0500

    Código base del proyecto de ventas BrensKi. Incluye la clase main SistemaVentasBrensKi y el JFrame del login

---
+++ src/main/java/com/brenski/sistemaventasbrenski/SistemaVentasBrensKi.java
@@ -0,0 +1,8 @@
+package com.brenski.sistemaventasbrenski;
+
+public class SistemaVentasBrensKi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
